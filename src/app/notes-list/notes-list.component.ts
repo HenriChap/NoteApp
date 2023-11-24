cfd0e851de662dd4017c63aedd27421c7f705585
HenriChap <henriquechapola@gmail.com>
2023-11-24 20:30:31 -0300

    Finish and fix notes-list.component.ts

--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NOTES } from '../../notes';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.css'
 })
 export class NotesListComponent {
+  notes = NOTES;
 
+  show(title:string){
+    alert(title);
+  }
 }
