80829c0755828cab73249de7296c6b227925bc1e
HenriChap <henriquechapola@gmail.com>
2023-11-24 20:42:42 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import{ NotesListComponent } from  './notes-list/notes-list.component';
 
 @Component({
   selector: 'app-root',
