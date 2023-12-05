93e0991800fe3824f11141ff333ab19f8813e8b5
HenriChap <henriquechapola@gmail.com>
2023-12-04 21:52:42 -0300

    Add commentaries to add-note.component.ts

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -12,25 +12,33 @@ import { NOTES } from '../../notes';
   styleUrl: './add-note.component.css'
 })
 export class AddNoteComponent {
+  // Criação de um novo formulário com dois campos: 'title' e 'text'. Ambos são obrigatórios.
   addNoteForm = new FormGroup({
     title: new FormControl('', Validators.required),
     text: new FormControl('', Validators.required)
   });
+  // Método para adicionar uma nova nota.
   addNote(){
+    // Recupera o valor dos campos 'title' e 'text' do formulário. Se algum campo estiver vazio, atribui uma string vazia.
     let title = this.addNoteForm.value.title ?? '';
     let text = this.addNoteForm.value.text ?? '';
+    // Verifica se o formulário é válido.
     if (this.addNoteForm.valid) {
+      // Cria um array com todos os IDs das notas existentes.
       let ids = NOTES.map((a) => a.id);
       let maxId = 0;
+      // Se existem IDs, encontra o maior.
       if (ids.length > 0) {
         maxId = Math.max(...ids)
       }
+      // Cria uma nova nota com um ID que é o maior ID + 1, o título e o texto do formulário, e um campo 'isDone' definido como false (caso contrário da erro por ser diferente da interface Note).
       let newNote = {
         id: maxId + 1,
         title: title,
         text: text,
         isDone: false
       };
+       // Adiciona a nova nota no fim do array de nota
       NOTES.push(newNote);
       this.addNoteForm.reset();
 
