e41ab0036a2bc89a1fa504a292adf40980337d4c
HenriChap <henriquechapola@gmail.com>
2023-12-14 01:04:19 -0300

    Import RouterModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import{ NotesListComponent } from  './notes-list/notes-list.component';
 import { AddNoteComponent } from './add-note/add-note.component';
@@ -8,7 +8,7 @@ import { AddNoteComponent } from './add-note/add-note.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, NotesListComponent, AddNoteComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, NotesListComponent, AddNoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NOTES } from '../../notes';
 import { NgFor } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, RouterModule],
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.css'
 })
